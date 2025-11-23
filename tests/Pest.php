11a242f98e8bca047ad1565f3a9605ca6aee403c
Leodyver Semilla <leodyversemilla07@gmail.com>
2025-11-23 12:52:20 +0800

    Add module permission seeders and RBAC tests, fix Pest Unit configuration

--- tests/Pest.php
+++ tests/Pest.php
@@ -19,6 +19,9 @@ pest()->extend(Tests\TestCase::class)
     ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Browser');
 
+pest()->extend(Tests\TestCase::class)
+    ->in('Unit');
+
 /*
 |--------------------------------------------------------------------------
 | Module Tests
