a77a520c32e755f360de89c009c46be39cb07ce9
Leodyver Semilla <leodyversemilla07@gmail.com>
2026-05-10 06:00:40 +0800

    fix: restore Pest.php structure - separate Library directive, remove Admission from Feature chain

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,8 +13,10 @@
 
 pest()->extend(Tests\TestCase::class)
     ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature')
-    ->in('Admission')
+    ->in('Feature');
+
+pest()->extend(Tests\TestCase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Library');
 
 pest()->extend(Tests\TestCase::class)
